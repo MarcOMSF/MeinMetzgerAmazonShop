9ba9e4f8a5b7b69db6170c825db2f7196587daa3
MarcOMSF <marco.frumento@yahoo.de>
2021-12-08 07:21:34 +0100

    addToBasket

---
+++ amazone-clone/src/firebase.js
@@ -0,0 +1,10 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+    apiKey: "AIzaSyCIAMeKbwTRBzGv65pDuaT0gkHrAiumVUQ",
+    authDomain: "challange-37cd7.firebaseapp.com",
+    projectId: "challange-37cd7",
+    storageBucket: "challange-37cd7.appspot.com",
+    messagingSenderId: "377310899580",
+    appId: "1:377310899580:web:84b969f0c01be0d7a3c8f9",
+    measurementId: "G-3715R9WKYL"
+  };
